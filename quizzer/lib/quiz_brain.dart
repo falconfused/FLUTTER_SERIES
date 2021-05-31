d36610dce150ebf7c027ea2beb5c8e91896e6eaf
falconfused <54740376+falconfused@users.noreply.github.com>
2021-05-31 22:29:33 +0530

    Updated the Question Bank

--- quizzer/lib/quiz_brain.dart
+++ quizzer/lib/quiz_brain.dart
@@ -2,64 +2,65 @@ import 'questionbank.dart';
 
 class QuizBrain {
   List<QuestionBank> QuestionBan = [
-    QuestionBank(q: "Venus is the closest planet to the Sun.", a: false),
+    QuestionBank(q: "1.Venus is the closest planet to the Sun.", a: false),
     QuestionBank(
-        q: "The tilt of Earth\'s axis has no effect on it\'s length of day",
+        q: "2.The tilt of Earth\'s axis has no effect on it\'s length of day",
         a: false),
-    QuestionBank(q: "There\'s a class of stars that are just 25ºC.", a: true),
+    QuestionBank(q: "3.There\'s a class of stars that are just 25ºC.", a: true),
     QuestionBank(
-        q: "'One small step for man' is the first thing anyone said on the moon.",
-        a: false),
-    QuestionBank(
-        q: "The Great Wall of China is the only human-made object you can see from space.",
+        q: "4.'One small step for man' is the first thing anyone said on the moon.",
         a: false),
     QuestionBank(
-        q: "There's a nebula shaped like a manatee that scientists named the 'Manatee Nebula' in a special ceremony in 2013.",
-        a: true),
-    QuestionBank(
-        q: "It takes 170,000 YEARS, on average, for a photon to travel from the centre of the sun to it's surface.",
-        a: true),
-    QuestionBank(
-        q: "The sun makes up 99.8% of the mass of the solar system.", a: true),
-    QuestionBank(
-        q: "The nearest black hole to Earth is just 1,600 light years away.",
+        q: "5.The Great Wall of China is the only human-made object you can see from space.",
         a: false),
     QuestionBank(
-        q: "Instead of spending money creating a pen that would work in space, the Russians just use pencils.",
-        a: true),
-    QuestionBank(q: "The sun is yellow.", a: false),
-    QuestionBank(
-        q: "If you went into space without a spacesuit on, you'd explode.",
+        q: "6.There's a nebula shaped like a manatee that scientists named the 'Manatee Nebula' in a special ceremony in 2013.",
         a: true),
     QuestionBank(
-        q: "Apollo astronauts collected Moon rocks for analysis. Astronauts haven't traveled to Mars to collect rocks, so we don't have any pieces of Mars to analyze.",
+        q: "7.It takes 170,000 YEARS, on average, for a photon to travel from the centre of the sun to it's surface.",
+        a: true),
+    QuestionBank(
+        q: "8.The sun makes up 99.8% of the mass of the solar system.",
+        a: true),
+    QuestionBank(
+        q: "9.The nearest black hole to Earth is just 1,600 light years away.",
         a: false),
     QuestionBank(
-        q: "Sunlight requires nearly 25 seconds to reach Earth.", a: false),
-    QuestionBank(
-        q: "Jupiter is more massive than all the other planets in our solar system put together",
+        q: "10.Instead of spending money creating a pen that would work in space, the Russians just use pencils.",
         a: true),
-    QuestionBank(q: " Blue stars are much cooler than red stars", a: false),
+    QuestionBank(q: "11.The sun is yellow.", a: false),
     QuestionBank(
-        q: "Approximately 90% of our galaxy might contain dark matter.",
+        q: "12.If you went into space without a spacesuit on, you'd explode.",
         a: true),
     QuestionBank(
-        q: "The Moon is larger in surface area than the United States",
-        a: true),
-    QuestionBank(
-        q: "Of the four inner planets (Mercury, Venus, Earth, and Mars), Mars has the coolest surface temperature",
+        q: "13.Apollo astronauts collected Moon rocks for analysis. Astronauts haven't traveled to Mars to collect rocks, so we don't have any pieces of Mars to analyze.",
         a: false),
     QuestionBank(
-        q: "Footprints and tyre tracks that have been left behind by astronauts on the moon will stay there forever.",
-        a: true),
-    QuestionBank(q: "Mars is sometimes known as the blue planet.", a: false),
-    QuestionBank(q: "A day on Venus is longer than a year.", a: true),
-    QuestionBank(q: " Astronauts grow in space.", a: true),
+        q: "14.Sunlight requires nearly 25 seconds to reach Earth.", a: false),
     QuestionBank(
-        q: "After their return from the moon, the crew of Apollo 11 were placed in quarantine.",
+        q: "15.Jupiter is more massive than all the other planets in our solar system put together",
+        a: true),
+    QuestionBank(q: "16. Blue stars are much cooler than red stars", a: false),
+    QuestionBank(
+        q: "17.Approximately 90% of our galaxy might contain dark matter.",
         a: true),
     QuestionBank(
-        q: "Mount Everest is the tallest mountain in the solar system. ",
+        q: "18.The Moon is larger in surface area than the United States",
+        a: true),
+    QuestionBank(
+        q: "19.Of the four inner planets (Mercury, Venus, Earth, and Mars), Mars has the coolest surface temperature",
+        a: false),
+    QuestionBank(
+        q: "20.Footprints and tyre tracks that have been left behind by astronauts on the moon will stay there forever.",
+        a: true),
+    QuestionBank(q: "21.Mars is sometimes known as the blue planet.", a: false),
+    QuestionBank(q: "22.A day on Venus is longer than a year.", a: true),
+    QuestionBank(q: "23. Astronauts grow in space.", a: true),
+    QuestionBank(
+        q: "24.After their return from the moon, the crew of Apollo 11 were placed in quarantine.",
+        a: true),
+    QuestionBank(
+        q: "25.Mount Everest is the tallest mountain in the solar system. ",
         a: false)
   ];
 }
